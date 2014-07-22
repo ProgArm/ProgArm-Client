c7b5469cfdcea61bcc5ec7338358bf19a2c74261
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-07-22 14:12:34 +0300

    Tiny progress.

--- progarm.pl
+++ progarm.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use 5.12.0;
 
 use Device::SerialPort;
 
@@ -10,7 +11,26 @@ $port->baudrate(38400);
 $port->parity("none");
 $port->stopbits(1);
 
+Action = (p => \&Ping, p => \&Pong);
+
+sub Ping {
+  return 1 if wantarray eq ''; # TODO is it the best way to check for false but NOT undefined?
+  ...
+}
+
+sub Pong {
+  return 1 if wantarray eq ''; # TODO use return value instead of context to determine required number of bytes?
+  say 'Pong received';
+}
+
+my @bytes = ();
+
 while(1) {
-  my $byte=$port->read(1);
-  print "$byte";
+  my $byte = $port->read(1);
+  say "$byte";
+  push @bytes, $byte;
+  if (scalar($bytes) >= Action{$bytes[0]}) {
+    &{$Action{$bytes[0]}}($bytes);
+  }
+  @bytes = ();
 }
