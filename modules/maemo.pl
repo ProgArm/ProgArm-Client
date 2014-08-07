df39b528523d5d901df0a4d22bb98a396a4782c5
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-08-07 03:49:45 +0300

    override CmusStart on maemo, maemo files merged

--- modules/maemo.pl
+++ modules/maemo.pl
@@ -4,12 +4,22 @@ use warnings;
 package ProgArm;
 our(%Keys, %CODES);
 
-# TODO ensure that we are running after amixer.pl
+@Keys{qw(CallAnswer CallRelease)} = ($CODES{w}, $CODES{r});
+
+sub CallAnswer {
+  `dbus-send --system --print-reply --dest=com.nokia.csd.Call /com/nokia/csd/call/1 com.nokia.csd.Call.Instance.Answer`;
+}
+
+sub CallRelease {
+  `dbus-send --system --print-reply --dest=com.nokia.csd.Call /com/nokia/csd/call com.nokia.csd.Call.Release`;
+}
+
+sub CmusStart {
+  `osso-xterm cmus &`;
+}
 
 sub VolumeMute {
   my $currentProfile = `dbus-send --type=method_call --print-reply=1 --dest=com.nokia.profiled /com/nokia/profiled com.nokia.profiled.get_profile`;
   my $newProfile = $currentProfile == 'silent' ? 'general' : 'silent';
   `dbus-send --type=method_call --dest=com.nokia.profiled /com/nokia/profiled com.nokia.profiled.set_profile string:$newProfile`;
 }
-
-
