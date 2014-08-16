9101862d96000b4ee25ba4e5af1da2714d7edf51
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-08-16 22:18:49 +0300

    New module: tutor.pl (Typing tutor)

--- modules/tutor.pl
+++ modules/tutor.pl
@@ -1,11 +1,54 @@
 use strict;
 use warnings;
+use v5.10;
 
 package ProgArm;
 our(%Keys, %CODES);
 
 $Keys{TutorMode} = $CODES{x};
 
+our $TutorMaxAttempts = 3;
+my $tutorOn;
+my $tutorChar;
+my $tutorAttempts;
+
 sub TutorMode {
-  # TODO
+  $tutorOn = !$tutorOn;
+  if ($tutorOn) {
+    Write("E0"); # disable device actions
+    Speak('Welcome to typing tutor!');
+    TutorRandom();
+  } else {
+    Speak('Goodbye!');
+    Write("E1"); # disable device actions
+  }
 }
+
+sub TutorRandom {
+  $tutorAttempts = 0;
+  $tutorChar = (keys %CODES)[rand keys %CODES];
+  Speak($tutorChar);
+}
+
+wrap CallAction, pre => \&Tutor;
+wrap UnknownAction, pre => \&Tutor;
+
+sub Tutor {
+  return unless $tutorOn;
+  $_[-1] = ''; # do not call original subroutine
+  my $action = shift;
+  if ($CODES{$tutorChar} == $action) {
+    Speak('Right!');
+    TutorRandom();
+  } else {
+    return TutorMode() if $CODES{'q'} == $action;
+    Speak('Wrong!');
+    $tutorAttempts++;
+    if ($tutorAttempts >= $TutorMaxAttempts) {
+      Speak('Lets try another!');
+      TutorRandom();
+    } else {
+      Speak($tutorChar);
+    }
+  }
+};
