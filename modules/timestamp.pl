dc6bb041dbcf9dad366fdda685f9dc117d839433
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-08-29 21:00:02 +0300

    New module: timestamp.pl (record current timestamp)

---
+++ modules/timestamp.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+use v5.10;
+
+use POSIX qw(strftime);
+
+package ProgArm;
+our(%Keys, $TimestampsFile);
+
+@Keys{SaveTimestamp} = 'w';
+$TimestampsFile = 'timestamps';
+
+sub SaveTimestamp {
+  return DismissLastTimestamp() if $_[0] ~~ -1;
+  open(my $fh, '>>', $TimestampsFile);
+  say $fh strftime("%Y-%m-%d %H:%M:%S", localtime);
+  close($fh);
+  Speak('Timestamp saved!');
+}
+
+sub DismissLastTimestamp {
+  open(my $fh, '>>', $TimestampsFile);
+  say $fh strftime("Dismiss! (%Y-%m-%d %H:%M:%S)", localtime);
+  close($fh);
+  Speak('Timestamp dismissed!');
+}
