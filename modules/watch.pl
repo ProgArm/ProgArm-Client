502ac4ba4c6b1b638464ec9f508d0e6c29077910
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-08-30 18:38:00 +0300

    Use strftime in watch.pl

--- modules/watch.pl
+++ modules/watch.pl
@@ -1,21 +1,20 @@
 use strict;
 use warnings;
+use v5.10;
+
+use POSIX qw(strftime);
 
 package ProgArm;
 our(%Keys);
 
 @Keys{qw(WatchTime WatchDate)} = qw(t d);
 
-my @days = qw(Monday Tuesday Wednesday Thursday Friday);
-
 sub WatchTime {
   return WatchDate() if $_[0] ~~ -1;
-  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-  Speak(($hour < 10 ? '0' : '') . $hour . ':' . ($min < 10 ? '0' : '') . $min); # e.g. 05:03
+  Speak(strftime("%H:%M", localtime));
 }
 
 sub WatchDate {
   return WatchTime() if $_[0] ~~ -1;
-  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-  Speak("$mday $days[$wday - 1] $mon");
+  Speak(strftime("%d %A %m)", localtime));
 }
