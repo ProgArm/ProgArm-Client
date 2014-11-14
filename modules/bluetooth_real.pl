13d9f7ba91e44bec2f89cc9211a85e32d3d62c93
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-11-14 19:01:15 +0200

    New module: bluetooth_real.pl (using Net::Bluetooth to create bluetooth connection)

---
+++ modules/bluetooth_real.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+use v5.10;
+
+package ProgArm;
+our($BluetoothAddress, $BluetoothPort);
+
+use Net::Bluetooth;
+use IO::Handle;
+
+$BluetoothAddress = '00:13:12:25:47:81';
+$BluetoothPort = 1;
+
+sub InitConnection {
+  my $obj = Net::Bluetooth->newsocket("RFCOMM");
+  return undef unless defined $obj;
+  return undef if $obj->connect($BluetoothAddress, $BluetoothPort) != 0;
+  *SERVER = $obj->perlfh();
+}
+
+my $first = 1;
+
+sub Read {
+  my $count = $_[0] // 1;
+  my $buf = '';
+  my $amount = read(SERVER, $buf, $count);
+  return ($amount, split(//, $buf)) if wantarray;
+  return $buf;
+}
+
+sub Write {
+  print SERVER $_[0];
+  SERVER->flush();
+}
+
+# close(SERVER); # should we do that ?
