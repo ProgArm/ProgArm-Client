ca92e4f4b108a79f9135945b7078652a5fd8ed5d
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-11-17 20:59:47 +0200

    Better example config

--- config.pl
+++ config.pl
@@ -6,5 +6,6 @@ package ProgArm;
 
 # Here you can modify default variables!
 # For example:
-#   our($UpdateInterval);
-#   $UpdateInterval = 1000;
+
+our($BluetoothAddress);
+$BluetoothAddress = '00:13:12:25:47:81';
