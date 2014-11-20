8f116ce46731c6492c15f091e40a4d8b61ed6d46
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-11-20 23:41:09 +0200

    Slightly better logging

--- modules/log.pl
+++ modules/log.pl
@@ -5,9 +5,16 @@ use Hook::LexWrap;
 
 package ProgArm;
 
-my $LogTimestampFormat = "%Y-%m-%d %H:%M:%S %s";
+my $LogTimestampFormat = "%Y-%m-%d %H:%M:%S %s ";
 
-wrap CallAction, post => sub {
+# XXX this module is designed to log exclusively useful data for stats
+# therefore we don't need any logging library (probably?)
+
+sub Log {
   open(my $fh, '>>', 'log') or die "cannot open log file: $!"; # XXX don't open it each time?
-  say $fh strftime($LogTimestampFormat, localtime), " Action($_[0])"
-};
+  say $fh strftime($LogTimestampFormat, localtime), @_;
+}
+
+wrap CallAction, post => sub { Log 'Action ', $_[0] };
+wrap OnConnect, post => sub { Log 'Connected' };
+wrap OnDisconnect, post => sub { Log 'Disconnected' };
