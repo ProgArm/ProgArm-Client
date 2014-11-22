d67728474ebf1a3912444774be25df5ee2aba530
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-11-22 23:19:23 +0200

    Corrections ('V' should be used instead of 'v', fixed subroutine name)

--- modules/debug.pl
+++ modules/debug.pl
@@ -7,12 +7,12 @@ use POSIX qw(strftime);
 package ProgArm;
 our(%Keys);
 
-@Keys{qw(SendFakeCommand Send)} = qw(u o);
+@Keys{qw(SendFakeCommand StartVibrating)} = qw(u o);
 
 sub SendFakeCommand {
   Write('l' . chr(7));
 }
 
 sub StartVibrating {
-  Write('v' . chr(255));
+  Write('V' . chr(255));
 }
