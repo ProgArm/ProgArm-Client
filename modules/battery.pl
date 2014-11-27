9527032e0ab598891bd407fb3459b7d0aba5e62e
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-11-27 21:09:38 +0200

    Separated battery logging from battery.pl to log.pl

--- modules/battery.pl
+++ modules/battery.pl
@@ -6,34 +6,38 @@ use Hook::LexWrap;
 use POSIX qw(strftime);
 
 package ProgArm;
-our(%Keys, %Commands, $BatteryLogInterval, $BatteryLogFile, $ChargePrescaler, $LogDir);
+our(%Keys, %Commands, $BatteryLogInterval, $ChargePrescaler, $LogDir,
+    $BatteryVoltage, $BatteryCharge, $BatteryTemperature);
 
 $Keys{TellBatteryInfo} = qw(g);
 $Commands{B} = \&ParseBatteryInfo;
 $BatteryLogInterval = 30; # in seconds
-$BatteryLogFile = "$LogDir/battery";
 $ChargePrescaler = 32;
 my $lastLogTime = 0; # in seconds
 
+# These will be set after we receive some info
+$BatteryVoltage = 0;
+$BatteryCharge = 0;
+$BatteryTemperature = 0;
+
 sub TellBatteryInfo {
   Write('b');
+  # TODO
 }
 
 sub ParseBatteryInfo {
+  say 'hello';
   return 0 if defined wantarray;
-  my $charge = (ord(Read()) << 8) + ord Read();
-  my $voltage = (ord(Read()) << 8) + ord Read();
-  my $temperature = (ord(Read()) << 8) + ord Read();
-  my $realCharge = 0.085 * $ChargePrescaler / 128 * $charge;
-  my $realVoltage = 6 * $voltage / 0xFFFF;
-  my $realTemperature = 600 * $temperature / 0xFFFF - 273.15;
+  my $charge = (ord(Read()) << 8) + ord Read(); # raw data
+  my $voltage = (ord(Read()) << 8) + ord Read(); # raw data
+  my $temperature = (ord(Read()) << 8) + ord Read(); # raw data
+  $BatteryCharge = 0.085 * $ChargePrescaler / 128 * $charge;
+  $BatteryVoltage = 6 * $voltage / 0xFFFF;
+  $BatteryTemperature = 600 * $temperature / 0xFFFF - 273.15;
   #printf "Charge: %.3f mAh (%s)\n", $realCharge, $charge;
   #printf "Voltage: %.3f V (%s)\n", $realVoltage, $voltage;
   #printf "Temperature: %.3f °C (%s)\n", $realTemperature, $temperature;
-
-  open(my $batteryFile, '>>', $BatteryLogFile) || die "Failed open battery log file: $!";
-  printf $batteryFile "%d %f %d %f %d %f %d\n", time(), $realCharge, $charge, $realVoltage, $voltage, $realTemperature, $temperature;
-  close $batteryFile || die "Failed to close battery log file: $!";
+  say 'bye';
 }
 
 wrap Update, post => sub {
--- modules/log.pl
+++ modules/log.pl
@@ -4,18 +4,22 @@ use v5.10;
 use Hook::LexWrap;
 
 package ProgArm;
-our($LogDir);
+our($LogDir, $BatteryVoltage, $BatteryCharge, $BatteryTemperature);
 
 my $LogTimestampFormat = "%Y-%m-%d %H:%M:%S %s ";
 
 # XXX this module is designed to log exclusively useful data for stats
 # therefore we don't need any logging library (probably?)
 
-sub Log {
-  open(my $fh, '>>', "$LogDir/log") or die "cannot open log file: $!"; # XXX don't open it each time?
-  say $fh strftime($LogTimestampFormat, localtime), @_;
+sub WriteLog {
+  open(my $fh, '>>', "$LogDir/$_[0]") or die "Cannot open log file: $!"; # XXX don't open it each time?
+  say $fh strftime($LogTimestampFormat, localtime), @_[1 .. @_ - 1];
 }
+
+sub Log { WriteLog('log', @_) }
+sub LogBattery { WriteLog('battery', @_) }
 
 wrap CallAction, post => sub { Log 'Action ', $_[0] };
 wrap OnConnect, post => sub { Log 'Connected' };
 wrap OnDisconnect, post => sub { Log 'Disconnected' };
+wrap ParseBatteryInfo, post => sub { LogBattery $BatteryCharge, $BatteryVoltage, $BatteryTemperature }; # TODO check it
