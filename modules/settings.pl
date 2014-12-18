6d0b49d78e6549bfc5c563a7ac37c52fb5c47e02
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-12-18 22:34:26 +0200

    More progress on settings.pl

--- modules/settings.pl
+++ modules/settings.pl
@@ -3,10 +3,14 @@ use warnings;
 use v5.10;
 
 package ProgArm;
-our(%Keys, %Commands);
+our(%Keys, %Commands, $SettingsFile, %Settings);
 
 $Keys{SendSettings} = qw(S);
 $Commands{s} = \&ReadSettings;
+$SettingsFile = 'settings';
+%Settings = ();
+
+LoadSettings();
 
 sub SendSettings {
   Write('TODO'); # TODO
@@ -14,4 +18,18 @@ sub SendSettings {
 
 sub ReadSettings {
   Read(); # TODO
+}
+
+sub SaveSettings {
+  open(my $fh, '>', $SettingsFile) or die "Cannot open settings file: $!";
+  for (sort keys %Settings) {
+    say $fh $_, '=', $Settings{$_}; # XXX "Inappropriate ioctl for device" - what?
+  }
+}
+
+sub LoadSettings {
+  open(my $fh, '<', $SettingsFile) or die "Cannot open settings file: $!";
+  while (<$fh>) {
+    $Settings{$1} = $2 if /([^=\s]+)\s*=\s*([^\s]+)/;
+  }
 }
